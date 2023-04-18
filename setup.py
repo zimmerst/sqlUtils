d15b0785537bf1164a45937369b58ffa4e6d3c99
Stephan Zimmer <zimmerst@gmail.com>
2023-04-18 18:50:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='sqlUtils',
-      version='1.0',
-      description='Tools for working with SQL data helping to analyze and visualize lengthy SQL scripts',
-      url='https://github.com/zimmerst/sqlUtils',
-      author='Stephan Zimmer',
-      author_email='zimmerst@gmail.com',
-      license='MIT',
-      packages=['sqlUtils'],
-      install_requires=[
-          'sqlparse',
-          'sql-metadata',
-          'graphviz',
-          'colorcet'
-      ],
-      zip_safe=False)
-
+setup(
+    name='sqlUtils',
+    version='0.1.0',
+    description='Utility package for working with SQL data transformations',
+    long_description='...',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/sqlUtils',
+    packages=find_packages(),
+    install_requires=[
+        'sqlparse',
+        'sql_metadata',
+        'graphviz',
+        'colorcet',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vizSQL=sqlUtils.vizSQL:main'
+        ],
+    },
+)
